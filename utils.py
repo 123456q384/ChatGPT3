4029baaf0e5c69c6c99178032983161c873aeb3b
123456q384 <993233906@qq.com>
2025-03-04 10:25:48 +0800

    Add files via upload

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ from langchain.memory import ConversationBufferMemory
 
 
 def get_chat_response(prompt, memory, openai_api_key):
-    model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key=openai_api_key)
+    model = ChatOpenAI(model="gpt-3.5-turbo", openai_api_key="sk-VllibgOnUePUv84AG1u9ASKMEdJWIJtJluyX7UtH1yWGS60R")
     chain = ConversationChain(llm=model, memory=memory)
 
     response = chain.invoke({"input": prompt})
