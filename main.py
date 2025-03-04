2a9ba2ada3e51775e4340af20ad0686a73de6c0a
123456q384 <993233906@qq.com>
2025-03-04 10:42:46 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ with st.sidebar:
 if "memory" not in st.session_state:
     st.session_state["memory"] = ConversationBufferMemory(return_messages=True)
     st.session_state["messages"] = [{"role": "ai",
-                                     "content": "陈俊贤，你好，我是小蓝的AI助手，你可以叫我DeepBlue，请问有什么可以帮你的吗？"}]
+                                     "content": "小柔姐姐，你好，我是小蓝的AI助手，你可以叫我DeepBlue，请问有什么可以帮你的吗？"}]
 
 for message in st.session_state["messages"]:
     st.chat_message(message["role"]).write(message["content"])
